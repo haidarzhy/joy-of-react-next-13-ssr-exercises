10336c43ea20288783def097778bfc698137c225
Arkadiy Haidarzhy <arkadiy@byzantium.com.ua>
2023-10-21 17:45:52 +0300

    exercise 3

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,37 +1,37 @@
 'use client';
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+import {useMediaQuery} from 'react-responsive';
 
 import Interview from './Interview';
 import './styles.css';
 
-function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
-  });
+function HiddenOnSmall({children}) {
+    return <div className="hidden-less-than-500">{children}</div>;
+}
 
-  return (
-    <main>
-      <Interview />
-      {isDesktop && (
-        <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
-          <h2>About the Artist</h2>
-          <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
-          </p>
-        </aside>
-      )}
-    </main>
-  );
+function InterviewExercise() {
+    return (
+        <main>
+            <Interview/>
+            <HiddenOnSmall>
+                <aside>
+                    <img
+                        src="/gwen-artist.png"
+                        alt="Portrait of the artist"
+                    />
+                    <h2>About the Artist</h2>
+                    <p>
+                        Gwen Altaria is a contemporary artist known for
+                        her unique blend of traditional oil painting
+                        techniques and pop culture references. Born and
+                        raised in London, England, Vivi had an early
+                        love for both the fine arts and the fantastical
+                        worlds of video games.
+                    </p>
+                </aside>
+            </HiddenOnSmall>
+        </main>
+    );
 }
 
 export default InterviewExercise;
